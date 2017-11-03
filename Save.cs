6445eef2b2673089130df334c57b2f87dd82a962
JD <john.dederer@protonmail.ch>
2017-11-03 21:33:17 +0100

    Project cleanup

---
+++ Save.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Blueprints
+{
+    class Save : MonoBehaviour
+    {
+        [ModAPI.Attributes.ExecuteOnGameStart]
+        private static void Init()
+        {
+            GameObject go = new GameObject("__Save__");
+            go.AddComponent<Save>();
+        }
+
+        //private void OnGUI()
+        //{
+        //
+        //}
+
+        private void Update()
+        {
+            if (ModAPI.Input.GetButtonDown("Save_State", "Blueprints"))
+            {
+                ModAPI.Log.Write("Saved current state");
+
+                TheForest.Utils.LocalPlayer.Stats.JustSave();
+            }          
+        }
+    }
+}
